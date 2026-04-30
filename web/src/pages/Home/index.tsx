253abaa3a4cf4e5dd0897564e0ec56c9d908c3fa
Erick Mo <mo@thunderlab.id>
2026-04-30 13:55:54 +0800

    feat(web): assemble full home page from section components

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,1 +1,29 @@
-export function Home() { return <div>Home</div> }
+import { Hero } from './Hero'
+import { CertBand } from './CertBand'
+import { PartnerList } from './PartnerList'
+import { CourseTicker } from './CourseTicker'
+import { BatchSection } from './BatchSection'
+import { FeaturesSection } from './FeaturesSection'
+import { TestimonialSection } from './TestimonialSection'
+import { B2BSection } from './B2BSection'
+import { BlogPreviewSection } from './BlogPreviewSection'
+import { CtaBand } from './CtaBand'
+import { FaqSection } from './FaqSection'
+
+export function Home() {
+  return (
+    <>
+      <Hero />
+      <CertBand />
+      <PartnerList />
+      <CourseTicker />
+      <BatchSection />
+      <FeaturesSection />
+      <TestimonialSection />
+      <B2BSection />
+      <BlogPreviewSection />
+      <CtaBand />
+      <FaqSection />
+    </>
+  )
+}
